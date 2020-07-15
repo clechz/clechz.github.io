edc1e08e53791d86e3d29db3c9af8470471338d9
MrAlzhrani <MrAlzhrani@users.noreply.github.com>
2020-07-15 12:13:18 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -2,43 +2,43 @@
 #include<stdio.h>
 #include<math.h>
 
-int main (void)
+int main(void)
 {
     float dollar;
-    int cents, coins;
+    int cents, coins; // all variabels
     do
     {
-    dollar = get_float ("enter your change:");
+        dollar = get_float("enter your change:"); // users change input
     }
     while (dollar <= 0);
     
-    cents = round (dollar*100);
-    coins = 0;
+    cents = round(dollar * 100);
+    coins = 0; //variabels assignment 
     
-    while (cents >= 25)
+    while (cents >= 25) //25 cents
     {
-       cents -= 25;
-       coins++;
+        cents -= 25;
+        coins++;
     }
-    while (cents >= 10)
+    while (cents >= 10) //10 cents
     {
         
         cents -= 10;
         coins++;
     }
-    while (cents >= 5)
+    while (cents >= 5) //5 cents
     {
         
         cents -= 5;
         coins++;
     }
-    while (cents >= 1)
+    while (cents >= 1) // 1 cent
     {
         
         cents -= 1;
         coins++;
     }
 
-   printf ("you will need at least %i coins\n", coins);
+    printf("you will need at least %i coins\n", coins); //coins output  
     
 }

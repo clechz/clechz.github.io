2eb980452fb34bfb87ce755ac2d8bb0f40d78c1a
MrAlzhrani <MrAlzhrani@users.noreply.github.com>
2020-07-22 13:04:35 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,58 +4,46 @@
 #include<string.h>
 #include<ctype.h>
 
-bool check_valid_key(string s);
 
 int main(int argc, char** argv)
 {
-    if (argc != 2 || !check_valid_key(argv[1]))
+    
+    int key = atoi(argv[1]); //"13">>13
+
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    
+    if (key < 0)
+    {
+        printf("key must be positve\n");
+        return 1;
+    }
 
-    int key = atoi(argv[1]); //"13">>13
-
+    
     string plaintext = get_string("plaintext: ");
 
-    printf("ciphertext: ");
+
     for (int i = 0, len = strlen(plaintext); i < len; i++)
     {
         char c = plaintext[i];
-        if (isalpha(c))
-        {
-            
-            if(islower(c))
+
+        if(islower(c))
         {
             printf("%c", (c - 'a' + key) % 26 + 'a');
         }
-        }
-
         else if(isupper(c))
         {
-            printf("%c", (c - 'A' + key) % 26 + 'A');          }
-        
+            printf("%c", (c - 'A' + key) % 26 + 'A');          
+        }
         else
         {
             printf("%c", c);
-        }
-
-
-
-
-
+        }    
     }
     printf("\n");
 }
 
-bool check_valid_key(string s)
-{
-    for (int i = 0, len = strlen(s); i < len; i++)
-        if (!isdigit(s[i]))
-        {
-            return false;
 
-        }
-        return true;
-
-    }

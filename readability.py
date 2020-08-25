5b46b2cca439d57c0ff45a2b524682417395a219
MrAlzhrani <MrAlzhrani@users.noreply.github.com>
2020-08-25 21:25:30 +0000

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -1,11 +1,11 @@
 from cs50 import get_string
-s = get_string("Text: ")
+s = get_string("Text: ").strip()
 nol = 0
 now = 0
 nos = 0
 
 for i in range(len(s)):
-    if  i == 0 and s[i] != ' ' or i != len(s) - 1 and s[i] == ' ' and s[i + 1] != ' ':
+    if (i == 0 and s[i] != ' ' or i != len(s) - 1 and s[i] == ' ' and s[i + 1] != ' '):
         now += 1
         
     if s[i].isalpha():
@@ -23,4 +23,4 @@ if index < 1:
 if index >= 16:
     print("grade 16+")
 else:
-    print(f"grade {index}")
+    print(f"Grade {index}")

3a5307b7ff5ac4da9365d1ea1fae9444db520a27
MrAlzhrani <MrAlzhrani@users.noreply.github.com>
2020-08-25 21:31:29 +0000

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -1,9 +1,9 @@
 from cs50 import get_string
 s = get_string("Text: ").strip()
-nol = 0
-now = 0
-nos = 0
-
+nol = 0  # leters
+now = 0  # words
+nos = 0  # sentance
+# iterit inside every char
 for i in range(len(s)):
     if (i == 0 and s[i] != ' ' or i != len(s) - 1 and s[i] == ' ' and s[i + 1] != ' '):
         now += 1
@@ -13,11 +13,11 @@ for i in range(len(s)):
         
     if s[i] == '!' or s[i] == '.' or s[i] == '?':
         nos += 1
-        
+# the algorthimatic formula        
 L = nol / now * 100
 S = nos / now * 100
 index = round(0.0588 * L - 0.296 * S - 15.8)
-
+# the output
 if index < 1:
     print("Before Grade 1")
 if index >= 16:

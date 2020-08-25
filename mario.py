30fe5e2c1aec227bdd4e7bfc5109efe6b50dff87
MrAlzhrani <MrAlzhrani@users.noreply.github.com>
2020-08-25 13:40:22 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -5,9 +5,9 @@ while True:
     if n >= 1 and n <= 8:
         break
 k = 1  # icrement of hashes
-s = n - 1 # num of spaces
+s = n - 1  # num of spaces
 # all of the nested loops
-for i in range(1,n+1,1):
+for i in range(1, n+1, 1):
     for s in range(s):
         print(' ', end='')
     s -= 1   

8adb1423a798420982bd5b0bb79dfe34bb48596b
MrAlzhrani <MrAlzhrani@users.noreply.github.com>
2020-09-04 09:52:35 +0000

    automated commit by check50 [check50=True]

--- 9.sql
+++ 9.sql
@@ -1,4 +1,5 @@
-SELECT COUNT(DISTINCT(people.name))
-FROM people JOIN stars, movies ON stars.person_id = people.id AND stars.movie_id = movies.id
+SELECT COUNT (DISTINCT name)
+FROM people JOIN stars ON stars.person_id = people.id 
+JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year = 2004
 ORDER BY people.birth
